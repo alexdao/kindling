035058073f0d9e569c33b8b071b7a75e5e184460
kyh8@duke.edu <kyh8@duke.edu>
2016-11-19 17:30:57 -0500

    Setup react component

---
+++ src/browser_action/js/App.js
@@ -0,0 +1,15 @@
+const React = require('react');
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello Kindlers</div>
+    );
+  }
+}
+
+module.exports = App;
